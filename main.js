c7c06953561c6d76b674dc8ba1fea57cc93a429c
Sanghoon Choi <sanghoon5499@gmail.com>
2022-05-12 07:19:12 -0400

    added some glb files

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.BasicShadowMap;
 
-camera.position.setZ(5.3);
-camera.position.setY(0) // 5+ deskheight
+camera.position.setZ(5.8);
+camera.position.setY(5+ deskHeight) // 5+ deskheight
 camera.position.setX(0);
 camera.rotation.x = -0.08;
 
@@ -40,7 +40,7 @@ function handle_load(gltf) {
   
   monitorMesh.scale.set(monitorMesh.scale.x * 0.55, monitorMesh.scale.y * 0.55, monitorMesh.scale.z * 0.55);
   monitorMesh.position.x = 0;
-  monitorMesh.position.y = 2.6;//5.1
+  monitorMesh.position.y = 5.1;
   monitorMesh.position.z = 1;
   monitorMesh.rotation.z = 3.1415;
   monitorMesh.side = THREE.DoubleSide;
@@ -53,11 +53,12 @@ loader.load( 'keyboard1.glb', handle_load2);
 function handle_load2(gltf) {
   const keyboardMesh = gltf.scene.children[0];
   
-  keyboardMesh.scale.set(keyboardMesh.scale.x * 0.55, keyboardMesh.scale.y * 0.55, keyboardMesh.scale.z * 0.55);
-  keyboardMesh.position.x = 0;
-  keyboardMesh.position.y = 2.6;//5.1
-  keyboardMesh.position.z = 1.5;
-  keyboardMesh.rotation.z = 3.1415;
+  keyboardMesh.scale.set(keyboardMesh.scale.x * 0.42, keyboardMesh.scale.y * 0.42, keyboardMesh.scale.z * 0.42);
+  keyboardMesh.position.x = -0.4;
+  keyboardMesh.position.y = 5.37;
+  keyboardMesh.position.z = 0.2;
+  keyboardMesh.rotation.z = 3.1415; 
+  keyboardMesh.rotation.x = 1.62;
   keyboardMesh.side = THREE.DoubleSide;
 
   scene.add(keyboardMesh);
@@ -68,14 +69,43 @@ function handle_load3(gltf) {
   const mouseMesh = gltf.scene.children[0];
   
   mouseMesh.scale.set(mouseMesh.scale.x * 0.55, mouseMesh.scale.y * 0.55, mouseMesh.scale.z * 0.55);
-  mouseMesh.position.x = 2.5;
-  mouseMesh.position.y = 2.6;//5.1
-  mouseMesh.position.z = 2;
+  mouseMesh.position.x = 2;
+  mouseMesh.position.y = 5.2;
+  mouseMesh.position.z = 2.1;
   mouseMesh.rotation.z = 3.1415;
   mouseMesh.side = THREE.DoubleSide;
 
   scene.add(mouseMesh);
 }
+
+loader.load( 'github.glb', handle_load4);
+function handle_load4(gltf) {
+  const githubMesh = gltf.scene.children[0];
+  
+  githubMesh.scale.set(githubMesh.scale.x * 1.55, githubMesh.scale.y * 1.55, githubMesh.scale.z * 1.55);
+  githubMesh.position.x = -14;
+  githubMesh.position.y = 4;
+  githubMesh.position.z = -25;
+  githubMesh.rotation.z = 3.1415;
+  githubMesh.side = THREE.DoubleSide;
+
+  scene.add(githubMesh);
+}
+
+loader.load( 'linkedin.glb', handle_load5);
+function handle_load5(gltf) {
+  const linkedinMesh = gltf.scene.children[0];
+  
+  linkedinMesh.scale.set(linkedinMesh.scale.x * 1.5, linkedinMesh.scale.y * 1.5, linkedinMesh.scale.z * 1.5);
+  linkedinMesh.position.x = -4.5;
+  linkedinMesh.position.y = 4.4;//5.1
+  linkedinMesh.position.z = -25;
+  linkedinMesh.rotation.z = 3.1415;
+  linkedinMesh.side = THREE.DoubleSide;
+
+  scene.add(linkedinMesh);
+}
+
 
 
 
@@ -105,7 +135,7 @@ function handle_load3(gltf) {
 
 
 
-// Lights
+// Lights/Lighting
 
 //scene.add(directionalLight, directionalLight2, directionalLight3, directionalLight4);
 
@@ -163,19 +193,11 @@ scene.background = new THREE.Color(0xffecf2);
 
 
 // 1st phase elements
-const monitorL = new THREE.Mesh(new THREE.BoxGeometry(0.2, 3, 0.2), new THREE.MeshPhongMaterial({color: 0x848484}));
-const monitorR = new THREE.Mesh(new THREE.BoxGeometry(0.2, 3, 0.2), new THREE.MeshPhongMaterial({color: 0x848484}));
-const monitorB = new THREE.Mesh(new THREE.BoxGeometry(4.8, 0.2, 0.2), new THREE.MeshPhongMaterial({color: 0x848484}));
-const monitorB2 = new THREE.Mesh(new THREE.BoxGeometry(0.8, 2, 0.2), new THREE.MeshPhongMaterial({color: 0x848484}));
-const monitorB3 = new THREE.Mesh(new THREE.BoxGeometry(1.8, 0.1, 1.2), new THREE.MeshPhongMaterial({color: 0x848484}));
-const monitorT = new THREE.Mesh(new THREE.BoxGeometry(4.8, 0.2, 0.2), new THREE.MeshPhongMaterial({color: 0x848484}));
-const wallL = new THREE.Mesh(new THREE.BoxGeometry(10, 15, 0.2), new THREE.MeshPhongMaterial({color: 0xFFFFFF}));
-const wallR = new THREE.Mesh(new THREE.BoxGeometry(10, 15, 0.2), new THREE.MeshPhongMaterial({color: 0xFFFFFF}));
+const wallL = new THREE.Mesh(new THREE.BoxGeometry(10, 15, 0.2), new THREE.MeshLambertMaterial({color: 0xFAF9F6}));
+const wallR = new THREE.Mesh(new THREE.BoxGeometry(10, 15, 0.2), new THREE.MeshLambertMaterial({color: 0xFAF9F6}));
 const floor = new THREE.Mesh(new THREE.BoxGeometry(800, 1, 400), new THREE.MeshPhongMaterial({color: 0xFFB347}));
-
-const desk = new THREE.Mesh(new THREE.BoxGeometry(15, 0.2, 10), new THREE.MeshPhongMaterial({color: 0xFFFFFF}));
-const keyboard = new THREE.Mesh(new THREE.BoxGeometry(2, 0.1, 0.9), new THREE.MeshPhongMaterial({color: 0x848484}));
-const mouse = new THREE.Mesh(new THREE.BoxGeometry(0.3, 0.1, 0.5), new THREE.MeshPhongMaterial({color: 0x848484}));
+const desk = new THREE.Mesh(new THREE.BoxGeometry(15, 0.2, 10), new THREE.MeshLambertMaterial({color: 0xDEB887}));
+const screen = new THREE.Mesh(new THREE.PlaneGeometry(3.9, 2.7), new THREE.MeshBasicMaterial({color: 0x000000}));
 
 // 2nd phase elements
 const github = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshPhongMaterial({color: 0xFF6961}))
@@ -187,23 +209,14 @@ const arrow = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshPhong
 const projectsWall = new THREE.Mesh(new THREE.BoxGeometry(80, 1, 40), new THREE.MeshPhongMaterial({color: 0xFFB347}));
 
 // 1st phase positions
-monitorL.position.z = 0; monitorL.position.x = -2.3; monitorL.position.y = 5.5 + deskHeight;
-monitorR.position.z = 0; monitorR.position.x = 2.3; monitorR.position.y = 5.5 + deskHeight;
-monitorB.position.z = 0; monitorB.position.x = 0; monitorB.position.y = 4 + deskHeight;
-monitorB2.position.z = 0; monitorB2.position.x = 0; monitorB2.position.y = 3 + deskHeight;
-monitorB3.position.z = 0.25; monitorB3.position.x = 0; monitorB3.position.y = 3.15 + deskHeight;
-monitorT.position.z = 0; monitorT.position.x = 0; monitorT.position.y = 7 + deskHeight;
-monitorB.userData.name = 'MONITORB'
 
 wallL.position.z = -2; wallL.position.x = -5; wallL.position.y = 6 + deskHeight;
 wallR.position.z = -2; wallR.position.x = 5; wallR.position.y = 6 + deskHeight;
-
 floor.position.z = -10; floor.position.x = 0; floor.position.y = 2;
 floor.receiveShadow = true;
-
 desk.position.z = 3.75; desk.position.x = 0; desk.position.y = 3 + deskHeight;
-keyboard.position.z = 2; keyboard.position.x = -0.3; keyboard.position.y = 3.15 + deskHeight;
-mouse.position.z = 2; mouse.position.x = 1.2; mouse.position.y = 3.15 + deskHeight;
+screen.position.z = 0.32; screen.position.x = 0; screen.position.y = 5.6 + deskHeight;
+
 
 // 2nd phase positions
 github.position.z = -25; github.position.x = -14; github.position.y = 4;
@@ -230,10 +243,9 @@ arrow.userData.name = 'ARROW'
 projectsWall.position.z = -30; projectsWall.position.x = 0; projectsWall.position.y = 50;
 projectsWall.receiveShadow = true;
 
-scene.add(monitorL, monitorR, monitorB, monitorB2, monitorB3, monitorT);
 scene.add(wallL, wallR, floor);
-scene.add(desk, keyboard, mouse);
-scene.add(github, linkedin, email, arrow);
+scene.add(desk, screen);
+scene.add(email, arrow);
 
 
 
@@ -349,7 +361,7 @@ function animateOpening() {
 function animate() {
   requestAnimationFrame(animate);
 
-  // line 124 for controls
+  // line 188 for controls
   controls.update();
 
   renderer.render(scene, camera);
